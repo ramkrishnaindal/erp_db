53199fb6503599feff697f5a2d2f18895bd460ae
ramkrishnaind <ramkrishnaind@gmail.com>
2025-04-28 12:19:53 +0530

    Added pagination

--- src/routes/erp_collection_routes.js
+++ src/routes/erp_collection_routes.js
@@ -5,41 +5,70 @@ const ErpCollection = require("../models/erp_collection");
 // Get all documents
 router.post("/", async (req, res) => {
   try {
-    const { software_name, domain_name, topic_type } = req.body;
-    const domain = await ErpCollection.find({
+    const {
+      software_name,
+      domain_name,
+      topic_type,
+      page_num = 1,
+      page_size = 10,
+    } = req.body;
+    let results = [];
+    results = await ErpCollection.find({
       domain_name: {
         $regex: new RegExp("^" + domain_name.toLowerCase().trim(), "i"),
       },
       topic_type: {
         $regex: new RegExp("^" + topic_type.toLowerCase().trim(), "i"),
       },
-    });
-    let results = [];
-    if (domain && domain.length) {
-      results = domain.filter(
-        (x) =>
-          x.software_name.trim().toLowerCase() ===
-          software_name.trim().toLowerCase()
-      );
-      if (!results.length)
-        results = domain.filter(
-          (x) =>
-            x.version_name?.trim().toLowerCase() ===
-            software_name.trim().toLowerCase()
-        );
-      if (!results.length)
-        results = domain.filter(
-          (x) =>
-            x.event_name?.trim().toLowerCase() ===
-            software_name.trim().toLowerCase()
-        );
-      if (!results.length)
-        results = domain.filter(
-          (x) =>
-            x.article_title?.trim().toLowerCase() ===
-            software_name.trim().toLowerCase()
-        );
-    }
+      software_name: {
+        $regex: new RegExp("^" + software_name.toLowerCase().trim(), "i"),
+      },
+    })
+      .skip((page_num - 1) * page_size)
+      .limit(page_size);
+
+    if (!results.length)
+      results = await ErpCollection.find({
+        domain_name: {
+          $regex: new RegExp("^" + domain_name.toLowerCase().trim(), "i"),
+        },
+        topic_type: {
+          $regex: new RegExp("^" + topic_type.toLowerCase().trim(), "i"),
+        },
+        version_name: {
+          $regex: new RegExp("^" + software_name.toLowerCase().trim(), "i"),
+        },
+      })
+        .skip((page_num - 1) * page_size)
+        .limit(page_size);
+    if (!results.length)
+      results = await ErpCollection.find({
+        domain_name: {
+          $regex: new RegExp("^" + domain_name.toLowerCase().trim(), "i"),
+        },
+        topic_type: {
+          $regex: new RegExp("^" + topic_type.toLowerCase().trim(), "i"),
+        },
+        event_name: {
+          $regex: new RegExp("^" + software_name.toLowerCase().trim(), "i"),
+        },
+      })
+        .skip((page_num - 1) * page_size)
+        .limit(page_size);
+    if (!results.length)
+      results = await ErpCollection.find({
+        domain_name: {
+          $regex: new RegExp("^" + domain_name.toLowerCase().trim(), "i"),
+        },
+        topic_type: {
+          $regex: new RegExp("^" + topic_type.toLowerCase().trim(), "i"),
+        },
+        article_title: {
+          $regex: new RegExp("^" + software_name.toLowerCase().trim(), "i"),
+        },
+      })
+        .skip((page_num - 1) * page_size)
+        .limit(page_size);
 
     res.status(200).json(results);
   } catch (err) {
